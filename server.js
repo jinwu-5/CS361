b7c380443793287fbbce0cb0efc6ffefeab1946d
Jin <jinwu281@gmail.com>
2021-11-10 20:55:24 -0800

    add methods to read/write CSV file

--- server.js
+++ server.js
@@ -18,9 +18,55 @@ app.use(
 app.use(express.static("public"));
 
 let { PythonShell } = require("python-shell");
+const fs = require("fs");
+const csv = require("csv-parser");
 
 app.get("/analytics", (req, res) => {
-  PythonShell.run("forest_service.py", null, function (err, result) {});
+  const request = ["State", req.query.states];
+  const filename = "attack_request.csv";
+  console.log(request);
+  async function writeToCSVFile(request) {
+    fs.writeFile(filename, extractAsCSV(request), (err) => {
+      if (err) {
+        console.log("Error writing to csv file", err);
+      } else {
+        console.log("2nd call", extractAsCSV(request));
+        console.log(`saved as ${filename}`);
+      }
+    });
+  }
+
+  function extractAsCSV(request) {
+    return request.join(", ");
+  }
+
+  function wait(ms) {
+    return new Promise((r) => setTimeout(r, ms));
+  }
+
+  async function runPythonScript() {
+    await wait(50);
+    PythonShell.run("attack_service.py", null, function (err, result) {});
+    console.log("3rd call", "python ran");
+  }
+
+  var inputState = [];
+  async function readResponse() {
+    await wait(100);
+    fs.readFile("attack_response.csv", "utf8", (err, data) => {
+      if (err) return console.error("Error while opening file");
+      inputState = String(data).split(", ");
+      console.log("4th call", "send data");
+      res.send(inputState);
+    });
+  }
+
+  async function runPipeline() {
+    await writeToCSVFile(request);
+    await runPythonScript();
+    readResponse();
+  }
+  runPipeline();
 });
 
 app.listen(PORT, () => {
